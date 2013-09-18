7482176f2ab367fbd238260b4cfd895d5876e8e7
JW <j4wang@gmail.com>
2013-09-17 22:17:29 -0700

    Initial Commit

---
+++ Tip Calculator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Tip Calculator
+//
+//  Created by John on 9/17/13.
+//  Copyright (c) 2013 John. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
